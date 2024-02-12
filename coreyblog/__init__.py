adc0af0db0e610ddc8ca82b8516dca54bdd75a14
Griffiths, Daniel <Daniel.Griffiths@tatasteeleurope.com>
2024-02-12 16:22:42 +0100

    part 11: config file added, working app

--- coreyblog/__init__.py
+++ coreyblog/__init__.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from coreyblog.config import Config
+
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -16,11 +17,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'   # login route directs login manager to this route when a page needs a login
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
+
 mail = Mail(app)
 
 
